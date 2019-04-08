4d6db5e8f846571c4705f614e93e5c1c8c34c7d9
Alexey <aatryapko1201@gmail.com>
2019-04-08 22:29:37 +0300

    Add new features

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,32 +1,85 @@
 'use strict'
-import '../styles/index.scss';
 
-const links = [
-  "https://medium.com/{{route}}",
-  "https://translate.google.com/?source=gtx#view=home&op=translate&sl=en&tl=ru&text={{route}}",
-  "https://facebook.com/{{route}}",
-  "https://github.com/{{route}}",
-];
+const data = {};
+let links = [];
 
 const linksList = document.querySelector('.links-list');
+const presetsList = document.querySelector('.presets-list');
+const keyWordBtn = document.querySelector('.btn-key-word');
+const KeyWordInput = document.querySelector('.input-key-word');
+const PresetInput = document.querySelector('.input-preset-title');
+const LinkInput = document.querySelector('.input-link');
+const addPreset = document.querySelector('.preset-add');
+const deletePreset = document.querySelector('.preset-delete');
 
-const updateList = data => {
-  data.forEach(link => {
+const updateList = (data, parent, classes, clickCB) => {
+  while (parent.firstChild) parent.removeChild(parent.firstChild);
+  data.forEach(value => {
     const li = document.createElement('li');
-    li.classList.add('links-item')
-    li.innerHTML = link;
-    linksList.appendChild(li);
+    li.classList.add(...classes);
+    li.innerHTML = value;
+    li.addEventListener('click', clickCB);
+    parent.appendChild(li);
   });
 };
-updateList(links);
 
 const processing = (link, key) => link.split('{{route}}').join(key);
 
 const openWindows = (links, key) => links.map(link => processing(link, key))
   .forEach(link => window.open(link));
 
-const keyWordBtn = document.querySelector('.btn-key-word');
-const KeyWordInput = document.querySelector('.input-key-word');
+const linkItemClick = item => {
+  const value = item.target.outerText;
+  LinkInput.value = value;
+};
+
+const presetItemClick = preset => {
+  const value = preset.target.outerText;
+  PresetInput.value = value;
+  links = data[value];
+  updateList(links, linksList, ['links-item'], linkItemClick);
+};
+
+const config = {
+  apiKey: "AIzaSyB1jaS834z_XSQ9YHQ2L-N7aPsUjWHYc30",
+  authDomain: "searchservice-d0e77.firebaseapp.com",
+  databaseURL: "https://searchservice-d0e77.firebaseio.com",
+  projectId: "searchservice-d0e77",
+  storageBucket: "searchservice-d0e77.appspot.com",
+  messagingSenderId: "663524939814"
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+firebase.database().ref().on('value', snapshot => {
+  Object.assign(data, snapshot.val());
+  const presetsData = Object.keys(data);
+  updateList(presetsData, presetsList, ['preset-item'], presetItemClick);
+});
+
+addPreset.addEventListener('click', () => {
+  const key = PresetInput.value;
+  if (key && !data[key]) {
+    firebase.database().ref().update({
+      [key]: ['https://en.wikipedia.org/wiki/{{route}}']
+    });
+    PresetInput.value = "";
+  }
+});
+
+deletePreset.addEventListener('click', () => {
+  const key = PresetInput.value;
+  if (key && data[key]) {
+    firebase.database().ref().update({
+      [key]: null
+    });
+    PresetInput.value = "";
+    delete data[key];
+    console.table(data);
+    updateList(Object.keys(data), presetsList, ['preset-item'], presetItemClick)
+  }
+});
 
 keyWordBtn.addEventListener('click', () => openWindows(links, KeyWordInput.value));
 KeyWordInput.addEventListener('keypress', e => (
